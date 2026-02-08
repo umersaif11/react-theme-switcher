30f5928a6669d8b45c517eabdfbd0e60357fe38a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-08 14:23:36 +0500

    Export themecode variable and two theme methods from the create context

--- src/context/theme.js
+++ src/context/theme.js
@@ -1,5 +1,7 @@
 import { createContext, useContext } from "react";
 
 export const themeContext = createContext({
-    
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {}
 })
