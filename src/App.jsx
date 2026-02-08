7f6ebfe1b20ddc83d65e722ee2b52eb1d5f34f7b
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-08 15:49:06 +0500

    Wrap the jsx in the context provider and give it value attribute with values given as arguments while creating context

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 
 import './App.css'
+import { ThemeProvider } from './context/theme'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-[#172842] min-h-screen py-8">
-      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-          <div className="mb-4">
-              {/* Todo form goes here */} 
-          </div>
-          <div className="flex flex-wrap gap-y-3">
-              {/*Loop and Add TodoItem here */}
-          </div>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+            <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+            <div className="mb-4">
+                {/* Todo form goes here */} 
+            </div>
+            <div className="flex flex-wrap gap-y-3">
+                {/*Loop and Add TodoItem here */}
+            </div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
