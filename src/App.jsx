2dc5758e7715844e222a878ff7c3a330f795f164
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-08 17:47:49 +0500

    Remove the bug of not adding default value in usestate

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/Themebtn'
+import Card from './components/Card'
 
 function App() {
-  const [themeMode, setThemeMode] = useState
+  const [themeMode, setThemeMode] = useState("light")
   const lightTheme = () => {
     setThemeMode("light")
   }
@@ -24,11 +26,11 @@ function App() {
 <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            
+            <ThemeBtn/>
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-            
+            <Card/>
         </div>
     </div>
 </div>
